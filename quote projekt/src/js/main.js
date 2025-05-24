6112b21002bcbc9fb5f5f1667d37f1aed1e09170
bicskey-balazs <bicskey.balazs@students.jedlik.eu>
2025-05-24 18:45:54 +0200

    új alap

---
+++ quote projekt/src/js/main.js
@@ -0,0 +1,14 @@
+const test = document.querySelector("#test");
+const test2 = document.querySelector("#test2");
+
+fetch('https://api.allorigins.win/get?url=' + encodeURIComponent('https://zenquotes.io/api/random'))
+  .then(response => response.json())
+  .then(data => {
+    const parsedData = JSON.parse(data.contents); 
+    const quoteText = parsedData[0].q;
+    const author = parsedData[0].a;
+
+    test.innerHTML = quoteText;
+    test2.innerHTML = author;
+  })
+  .catch(error => console.error('Error fetching quote:', error));
