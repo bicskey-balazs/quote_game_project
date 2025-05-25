9f62892cf538f6f126df630a1a6d1eb1d849e2d5
anda-kristof <anda.kristof@students.jedlik.eu>
2025-05-25 18:53:00 +0200

    tiszta_kód_elve

--- quote_projekt/src/js/main.js
+++ quote_projekt/src/js/main.js
@@ -1,14 +1,27 @@
 const test = document.querySelector("#test");
 const test2 = document.querySelector("#test2");
+const beszeljgomb = document.querySelector("#beszelj")
 
-fetch('https://api.allorigins.win/get?url=' + encodeURIComponent('https://zenquotes.io/api/random'))
-  .then(response => response.json())
-  .then(data => {
-    const parsedData = JSON.parse(data.contents); 
-    const quoteText = parsedData[0].q;
-    const author = parsedData[0].a;
 
-    test.innerHTML = quoteText;
-    test2.innerHTML = author;
-  })
-  .catch(error => console.error('Error fetching quote:', error));
+function callQuote(){
+
+  fetch('https://api.allorigins.win/get?url=' + encodeURIComponent('https://zenquotes.io/api/random'))
+    .then(response => response.json())
+    .then(data => {
+      const parsedData = JSON.parse(data.contents); 
+      const quoteText = parsedData[0].q;
+      const author = parsedData[0].a;
+  
+      test.innerHTML = quoteText;
+      test2.innerHTML = author;
+    })
+    .catch(error => console.error('Error fetching quote:', error));
+}
+
+
+
+
+
+
+
+
